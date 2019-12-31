2840096ac54a34a20abb4aa3a777bfab9375b399
androidmalin <malin.myemail@163.com>
2019-12-31 23:52:56 +0800

    update shell

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 first_device=`adb devices | awk  'NR==2' | awk  '{print $1}'`
 packageName="com.malin.hook"
-echo "apk will install to "$first_device
-adb -s $first_device uninstall $packageName
+echo "apk will install to "${first_device}
+adb -s ${first_device} uninstall ${packageName}
 gradle -q app:installDebug -x lint --parallel --offline --continue &&
-adb -s $first_device shell am start $packageName/.MainActivity
+adb -s ${first_device} shell am start ${packageName}/.MainActivity
--- showlog.sh
+++ showlog.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 first_device=`adb devices | awk  'NR==2' | awk  '{print $1}'`
-adb -s $first_device logcat -c
-pidcat -s $first_device
+adb -s ${first_device} logcat -c
+pidcat -s ${first_device}
