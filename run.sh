256088dc0437628ae234c89bdf3064a0747b1896
androidmalin <malin.myemail@163.com>
2020-07-30 14:56:41 +0800

    remove unused kotlin dependencies

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ first_device=`adb devices | awk  'NR==2' | awk  '{print $1}'`
 packageName="com.malin.hook"
 echo "apk will install to "$first_device
 adb -s $first_device uninstall $packageName
-gradle -q app:installDebug -x lint --parallel --offline --continue &&
+gradle -q app:installDebug -x lint --parallel --continue &&
 adb -s $first_device shell am start $packageName/.MainActivity
