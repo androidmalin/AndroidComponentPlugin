268f562ad133b1edfa1a8d59aed4d84cb3d8d83e
androidmalin <malin.myemail@163.com>
2020-08-06 23:46:02 +0800

    upgrade the version of gradle and delete unused code

--- app/src/main/java/com/malin/hook/PluginResourceUtil.java
+++ app/src/main/java/com/malin/hook/PluginResourceUtil.java
@@ -5,6 +5,8 @@ import android.content.res.AssetManager;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 
+import androidx.core.content.res.ResourcesCompat;
+
 import java.io.File;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -33,7 +35,10 @@ public class PluginResourceUtil {
                 break;
             }
         }
-        return resources.getDrawable(resId);
+        if (resources != null) {
+            return ResourcesCompat.getDrawable(resources, resId, context.getTheme());
+        }
+        return null;
     }
 
 
