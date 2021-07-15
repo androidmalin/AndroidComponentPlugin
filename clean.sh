bef16919d20c0e5299332cd8bbe4dafa6e2964d9
androidmalin <malin.myemail@163.com>
2021-07-15 22:45:51 +0800

    update plugin apk file

--- clean.sh
+++ clean.sh
@@ -2,3 +2,5 @@
 rm -rf .idea/ .gradle/ 
 find . -name "build" -type d | xargs rm -rf
 find . -name "*.iml" -type f | xargs rm -rf
+find . -name ".cxx" -type d | xargs rm -rf
+find . -name "bin" -type d | xargs rm -rf
